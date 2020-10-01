dd54c95cb5cec426fe7e4d03efc634a5bf8e71c1
06fahad <fahad.rahman2813@gmail.com>
2020-10-01 14:40:19 -0400

    complete activity 2

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # ECE444-F2020-Lab3
 
+Mohammad Fahad Rahman
+
 This repo is a clone of: https://github.com/miguelgrinberg/flasky
+
+## Activity 1: 
+
+![activity 1 screenshot](Images/activity1.png)
+
+## Activity 2: 
+
+![activity 2 screenshot](Images/activity2_2.png)
+
+![activity 2 screenshot](Images/activity2_1.png)
+
+![activity 2 screenshot](Images/activity2_3.png)
+
+## Activity 3:
--- hello.py
+++ hello.py
@@ -1,12 +1,50 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, flash
 from flask_bootstrap import Bootstrap
 from datetime import date, datetime
+from flask_wtf import Form
+from wtforms import StringField, SubmitField 
+from wtforms.validators import Required, Email
+from flask import request
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'never_guess_this'
 bootstrap = Bootstrap(app)
-@app.route('/')
+
+
+
+class RegForm(Form):
+    name = StringField('What is your name?', validators = [Required()])
+    email = StringField('What is your UofT Email address?', validators = [Required(), Email()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods = ['POST', 'GET'])
 def index():
-    return '<h1>visit /user/"your name" route</h1>'
+    form = RegForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            old_name = session.get('name')
+            old_email = session.get('email')
+
+            if old_name is not None and old_name != form.name.data:
+                flash('Looks like you have changed your name!')
+            else:
+                session['name'] = form.name.data
+
+            if old_email is not None and old_email != form.email.data:
+                flash('Looks like you have changed your email!')
+            else:
+                session['email'] = form.email.data
+
+            if(form.email.data.endswith('@mail.utoronto.ca')):
+                corr_email = True
+            else:
+                corr_email = False
+            
+            return render_template('form.html', form=form, name=form.name.data, email=form.email.data, corr_email=corr_email)     
+        else:
+            return render_template('form.html', form=form)
+    else:
+        return render_template('form.html', form=form)
 
 @app.route('/user/<name>')
 def user(name):
